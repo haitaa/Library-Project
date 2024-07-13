b9c6d9ba2a9fd785e8e927df983103805ab28c41
Mustafa Haita <mustafahaitaa@gmail.com>
2024-07-13 17:11:17 +0300

    Brands component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import Header from "../components/Header/Header";
+import Brands from "../components/Brands/Brands";
 
 export default function Home() {
     return (
         <>
             <Header />
+            <Brands />
         </>
     );
 }
